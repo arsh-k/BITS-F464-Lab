77c9f7b6be40012276fe788ee22c25e151944d64
Arsh Kumbhat <f20190444@pilani.bits-pilani.ac.in>
2021-09-13 02:12:20 +0530

    problems left

--- Lab 2/ml_2.R
+++ Lab 2/ml_2.R
@@ -1,4 +1,7 @@
-N = 11 # Number of training data points
+# The below line can be typed on the console to clear all plots.
+dev.off(dev.list()["RStudioGD"])
+
+N = 100 # Number of training data points
 
 x = seq(0,1, length = N)
 
@@ -6,7 +9,7 @@ y = sin(2*pi*x)
 
 noise = y + rnorm(N, sd = 0.3)
 
-#plot(x, noise, pch = 19)
+plot(x, noise, pch = 19)
 
 lm1 <- lm(y~x)
 
@@ -27,11 +30,7 @@ xplot = seq(0, 1, length = 25)
 # In the below lineS change the model according to the 
 # polynomial fit you want to work with.
 
-#lines(xplot, predict(lm6, newdata=data.frame(x=xplot)), col = 'blue')
-
-# dev.off(dev.list()["RStudioGD"])
-# The above line can be typed on the console to clear all plots.
-
+lines(xplot, predict(lm6, newdata=data.frame(x=xplot)), col = 'blue')
 
 lms = list(lm1, lm2, lm3, lm4, lm5, lm6, lm7, lm8, lm9)
 
@@ -46,18 +45,60 @@ for(i in 1:9){
   
 }
 
-newy = sin(2*pi*x) + rnorm(11, sd=.3)
+newy = sin(2*pi*x) + rnorm(N, sd=.3)
 
 
 # Obtaining test error by finding variance.
 for (j in 1:9) {
   
-  test[j] = (1/10)*sum(newy - predict(lms[j][[1]], newdata=data.frame(x))^2)
+  test[j] = (1/10)*sum((newy - predict(lms[j][[1]], newdata=data.frame(x)))^2)
     
 }
 
-#lines(1:9, train, ylim=c(0, .5), col = 'black', lwd = 3)
-lines(1:9, test,pch='X', col = 'red', lwd = 3)
+plot(1:9, train,  xlab = 'Order of Polynomial' , ylab = 'Error - Test and Training', ylim = c(0,3))
+lines(1:9, train, col = 'black', lwd = 3)
+lines(1:9, test, pch='19', col = 'red', lwd = 3)
+cbind(train, test)
+
+
+summary(lm1)
+
+C1 <- coef(lm1)
+C2 <- coef(lm2)
+
+
+#Find out the maximum length of each coefficient list.
+max_length <- max(c(length(C1), length(C2)))
+
+
+# Build dataset setting the coefficient values for lower order models as NA.
+data <- data.frame(col1 = c(C1, rep(NA, max_length - length(C1))),
+                   col2 = c(C2, rep(NA, max_length - length(C2))))
+
+
+data
+
+#Regularization
+
+#Run the line below to install the glmnet package.
+# install.packages("glmnet", repos = "https://cran.us.r-project.org")
+
+library(glmnet)
+
+lambdas <- 10^seq(3, -2, by = -.1)
+lambdas
+
+x = matrix(seq(0,1, length = N))
+y = matrix(sin(2*pi*x))
+
+x = cbind(x, rep(1, N))
+
+fit <- glmnet(x, y, alpha = 0, lambda = lambdas)
+summary(fit)
+
+##EXERCISE##
+
+
 
 
 
